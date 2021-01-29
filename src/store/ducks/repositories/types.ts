6a3dc6e349ed1b56c4d31f22276fcc16bc7c2736
phamphuckhai <34903119+phamphuckhai@users.noreply.github.com>
2021-01-29 13:37:55 +0700

    first init

---
+++ src/store/ducks/repositories/types.ts
@@ -0,0 +1,28 @@
+import userEvent from "@testing-library/user-event";
+
+/**
+ * Action types
+ */
+export enum RepositoriesTypes {
+  LOAD_REQUEST = '@repositories/LOAD_REQUEST',
+  LOAD_SUCCCES = '@repositories/LOAD_SUCCCES',
+  LOAD_FAILURE = '@repositories/LOAD_FAILURE'
+}
+
+/**
+ * Data types
+ */
+export interface Repository {
+  id?: string
+  content?: string
+}
+
+/**
+ * State type
+ */
+export interface RepositoriesState {
+  readonly data: Repository[]
+  readonly loading: boolean
+  readonly error: boolean
+}
+
